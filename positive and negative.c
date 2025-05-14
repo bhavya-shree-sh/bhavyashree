f6deeb3f84cdacf283374c64a8126f26a749f06f
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 10:08:50 +0530

    Create positive and negative.c

---
+++ positive and negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float num;
+
+
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    
+    if (num > 0) {
+        printf("The number is positive.\n");
+    } else if (num < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
+Enter a number: 6
+The number is positive.

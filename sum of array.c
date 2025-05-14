d8e7abd36a6ffff49f9af522820e733fbb432eda
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 15:22:17 +0530

    Create sum of array.c

---
+++ sum of array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,arr[n],sum=0,i;
+    printf("enter the array element n");
+    scanf("%d",&n);
+    printf("enter the element\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[n]);
+        sum+=arr[n];
+        
+    }
+    printf("%d\n",sum);
+}
+
+
+
+enter the array element n4
+enter the element
+4
+5
+6
+7
+22

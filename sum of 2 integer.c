dcf3bca08444ad5d9bff4b877c3ea4723a36c8c1
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 09:35:30 +0530

    Create  sum of 2 integer.c

---
+++ sum of 2 integer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2,sum;
+    printf("enter the first integer:");
+    scanf("%d", &num1);
+     
+    printf("enter the second integer:");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("sum of two integer is %d", sum);
+}

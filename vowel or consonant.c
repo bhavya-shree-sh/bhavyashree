d0e392ae3c77a4b0fd810f6c113622c928ca60f1
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 10:05:42 +0530

    Create vowel or consonant.c

---
+++ vowel or consonant.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+
+    printf("Enter an alphabet: ");
+    scanf(" %c", &ch); 
+
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        
+        char lower = (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch;
+
+        
+        if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+            printf("%c is a vowel.\n", ch);
+        } else {
+            printf("%c is a consonant.\n", ch);
+        }
+    } else {
+        printf("%c is not an alphabet.\n", ch);
+    }
+
+    return 0;
+}
+Enter an alphabet: h
+h is a consonant.
+

71298ff3bf5527da25f06f2d74d61fcabb0c2801
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 09:24:40 +0530

    Create grade.c

---
+++ grade.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int marks;
+    printf("enter a marks: ");
+    scanf("%d", &marks);
+    if(marks>=84)
+    printf("the grade is A");
+    else if(marks>=70)
+    printf("the grade is B");
+    else if("marks>=50")
+    printf("the grade is C");
+    else if("marks>=35")
+    printf("the grade is D");   
+    
+}

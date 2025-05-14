a92bcddc2469e62d05898f82c7cc3e8cd595ee67
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 12:19:33 +0530

    Create 1D  array.c

---
+++ 1D  array.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int row;
+int shri[3] = 
+    {1,2,3};
+int main() {
+  for(row=0;row<3;row++)
+  {
+          printf("%d\n",shri[row]);
+      }
+  
+
+    return 0;
+}
+
+
+1
+2
+3

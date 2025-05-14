f55e89cce842f007e71e4f63e30aeed442ee689f
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 09:25:50 +0530

    Create age calculation.c

---
+++ age calculation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+int age;
+    printf("enter a age: ");
+    scanf("%d", &age);
+    if(age<=17)
+    printf("they are adults");
+    else if(age<=50)
+    printf("they are youngers ");
+    else {
+    printf("they are olders");
+    }
+    
+     
+    
+}

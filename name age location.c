afb489533c025496e3d57d30a4c5a3bb077f9a1d
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 09:30:00 +0530

    Create name age location.c

---
+++ name age location.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+char name[] = "shree"; 
+int age = 19;  
+char location[] = "hebbur";  
+printf("name: %s\n", name);  
+printf("age: %d\n", age); 
+printf("location: %s\n", location); 
+return 0;}

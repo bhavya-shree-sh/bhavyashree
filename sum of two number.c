d59a6f5cd91a5d2a04a03db292117346f9fb74d7
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-14 10:50:32 +0530

    Create sum of two number.c

---
+++ sum of two number.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+ int a,b,sum;
+ printf("enter the value of a and b");
+ scanf("%d%d",&a,&b);
+ sum = a+b;
+    printf("sum of two no i%d" , sum);
+
+}
+
+enter the value of a and b7 7
+sum of two no i14

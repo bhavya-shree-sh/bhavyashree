54c31d74995bc00fad2c97726247d13097b9d64a
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-15 11:29:12 +0530

    Create length of string.c

---
+++ length of string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char name[8];
+    printf("enter the name");
+    scanf("%s",&name);
+    int length = strlen(name);
+    printf("%d\n",length);
+}
+
+enter the namebhavya
+6

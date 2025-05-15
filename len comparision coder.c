6349d6600c36df1fbfa223442ef023c747671db7
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-15 11:45:07 +0530

    Create len comparision coder.c

---
+++ len comparision coder.c
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char name[8];
+    printf("enter the name");
+    scanf("%s",&name);
+    int length = strlen(name);
+    printf("%d\n",length);
+    strcpy(name,"bhavya");
+    int comparision = strcmp("bhavya","shree");
+    printf("%d\n",comparision);
+    strcat(name,"coder");
+    printf("%s\n",name);
+}
+
+enter the nameshree
+5
+-1
+bhavyacoder
+

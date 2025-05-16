c429169946c1d3483a53725edb6dfbbaa9c82428
bhavya-shree-sh <bhavyahanumanthraju445@gmail.com>
2025-05-16 10:24:40 +0530

    Create integer and floatvalue.c

---
+++ integer and floatvalue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int intvalue;
+    float floatvalue;
+    printf("enter the integer:");
+    scanf("%d",&intvalue);
+    printf("enter an float: ");
+    scanf("%d",&floatvalue);
+    printf("you entered the integer; %d\n", intvalue);
+     printf("you entered the float; %d\n", floatvalue);
+}
+
+enter the integer:6
+enter an float: 67.9
+you entered the integer; 6
+you entered the float; 159814304
+
